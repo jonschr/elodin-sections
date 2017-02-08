305439ffc1a5a03ced911e19a88dec4424d1b4f7
redblue <jon@redblue.us>
2017-02-07 18:57:46 -0600

    Stay out of .site-inner

--- lib/page-flexible-content.php
+++ lib/page-flexible-content.php
@@ -7,11 +7,14 @@
 //* The rb_sections_output_sections function is located in the template-common-functions file,
 //  to allow for use by other templates as well
 add_action( 'rb_flexible_content_sections', 'rb_sections_output_sections' );
+add_action( 'genesis_after_header', 'rb_flexible_after_header' );
 
 //* Build the page
 get_header();
 
-$context_prefix = 'page_flexible_content_';
-do_action( 'rb_flexible_content_sections', $context_prefix );
-
 get_footer();
+
+function rb_flexible_after_header() {
+	$context_prefix = 'page_flexible_content_';
+	do_action( 'rb_flexible_content_sections', $context_prefix );
+}
